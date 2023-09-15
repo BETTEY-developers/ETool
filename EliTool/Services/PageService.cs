66eeb598876b096a702b0c21a1a27c05490b2b58
秋风Elipese <caihongxuan2020@outlook.com>
2023-09-15 23:23:05 +0800

    Add a Tool (Obsoleting)

--- EliTool/Services/PageService.cs
+++ EliTool/Services/PageService.cs
@@ -17,6 +17,7 @@ public class PageService : IPageService
         Configure<MainViewModel, MainPage>();
         Configure<SettingsViewModel, SettingsPage>();
         Configure<JsonCSharpConverterViewModel, Views.ControlPage.DeveloperTools.JsonCSharpConverterPage>();
+        Configure<NumberConverterViewModel, NumberConverterPage>();
     }
 
     public Type GetPageType(string key)
---
+++ EliTool/Views/ControlPage/Developer-Tools/NumberConverterPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using EliTool.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace EliTool.Views;
+
+public sealed partial class NumberConverterPage : Page
+{
+    public NumberConverterViewModel ViewModel
+    {
+        get;
+    }
+
+    public NumberConverterPage()
+    {
+        ViewModel = App.GetService<NumberConverterViewModel>();
+        InitializeComponent();
+    }
+}
