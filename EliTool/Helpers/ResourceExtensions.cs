401c985e22826bd403b91185b63ec92791805d81
秋风Elipese <caihongxuan2020@outlook.com>
2023-10-15 21:08:43 +0800

    Localized

--- EliTool/Helpers/ResourceExtensions.cs
+++ EliTool/Helpers/ResourceExtensions.cs
@@ -1,10 +1,25 @@
-﻿using Microsoft.Windows.ApplicationModel.Resources;
+﻿using System.Diagnostics;
+using Microsoft.Windows.ApplicationModel.Resources;
 
 namespace EliTool.Helpers;
 
 public static class ResourceExtensions
 {
     private static readonly ResourceLoader _resourceLoader = new();
+    private static Dictionary<string, ResourceCandidate> _resources = new();
 
-    public static string GetLocalized(this string resourceKey) => _resourceLoader.GetString(resourceKey);
+    static ResourceExtensions()
+    {
+        var map = new ResourceManager().MainResourceMap;
+        for (uint i = 0; i < map.ResourceCount; i++)
+        {
+            try
+            {
+                _resources.Add(map.GetValueByIndex(i).Key, map.GetValueByIndex(i).Value);
+            }
+            catch { Debug.Print(i.ToString()); }
+        }
+    }
+
+    public static string GetLocalized(this string resourceKey,string culture = "Resources") => /*_resourceLoader.GetString(resourceKey)*/ _resources[culture + "/" + resourceKey].ValueAsString;
 }
