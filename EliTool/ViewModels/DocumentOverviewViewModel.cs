499ac403e304393b78bc7d51b8bbb86d08a965ef
秋风Elipese <caihongxuan2020@outlook.com>
2023-10-17 19:21:49 +0800

    change navigation to documents way

--- EliTool/ViewModels/DocumentOverviewViewModel.cs
+++ EliTool/ViewModels/DocumentOverviewViewModel.cs
@@ -2,18 +2,39 @@
 using CommunityToolkit.Mvvm.Input;
 using EliTool.Contracts.Services;
 using EliTool.Views;
+using EliTool.Views.Document;
 
 namespace EliTool.ViewModels;
 
+public enum Documents
+{
+    OfflineTool,
+    PartialOfflineTool,
+    OnlineTool
+}
+
 public partial class DocumentOverviewViewModel : ObservableRecipient
 {
+    private Dictionary<Documents, Type> DocumentTypes = new()
+    {
+        [Documents.OfflineTool] = typeof(OfflineToolsDocumentPage),
+        [Documents.PartialOfflineTool] = typeof(OnOffLineToolsDocumentPage),
+        [Documents.OnlineTool] = typeof(OnlineToolsDocumentPage)
+    };
+    private Dictionary<Documents, Type> DocumentViewModelTypes = new()
+    {
+        [Documents.OfflineTool] = typeof(OfflineToolsDocumentViewModel),
+        [Documents.PartialOfflineTool] = typeof(OnOffLineToolsDocumentViewModel),
+        [Documents.OnlineTool] = typeof(OnlineToolsDocumentViewModel)
+    };
+
     public DocumentOverviewViewModel()
     {
     }
 
     [RelayCommand]
-    public void NavigationTo(string type)
+    public void NavigationToDocument(int type)
     {
-        App.GetService<INavigationService>().NavigateTo(type);
+        App.GetService<INavigationService>().NavigateTo(DocumentViewModelTypes[(Documents)type].FullName);
     }
 }
