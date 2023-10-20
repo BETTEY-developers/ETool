22eded323bcf4288555f7912cb2efd0588ee49f5
秋风Elipese <caihongxuan2020@outlook.com>
2023-10-20 21:10:16 +0800

    Add "Loading" Page( But... is no used:( )

---
+++ EliTool/ViewModels/LoadingViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EliTool.ViewModels;
+
+public partial class LoadingViewModel : ObservableRecipient
+{
+    public LoadingViewModel()
+    {
+    }
+}
---
+++ EliTool/Views/LoadingPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using EliTool.Contracts.Services;
+using EliTool.Services;
+using EliTool.ViewModels;
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace EliTool.Views;
+
+public sealed partial class LoadingPage : Page
+{
+    public LoadingViewModel ViewModel
+    {
+        get;
+    }
+
+    public LoadingPage()
+    {
+        ViewModel = App.GetService<LoadingViewModel>();
+        InitializeComponent();
+    }
+
+    private void Page_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var timer = DispatcherQueue.CreateTimer();
+        timer.Interval = new(0,0,1);
+        timer.Tick += (sender, e) =>
+        {
+            (App.MainWindow.Content as Frame).GoBack();
+            App.GetService<INavigationService>().NavigateTo(typeof(MainViewModel).FullName!);
+            timer.Stop();
+        };
+        timer.Start();
+    }
+}
